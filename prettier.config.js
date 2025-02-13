de0fcb35a3c1fecbf95596ec13c33bbfae6b6481
Harsh V Srivastava <harshvsri@gmail.com>
2025-02-13 15:34:28 +0530

    Init

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+    plugins: ["prettier-plugin-tailwindcss"],
+    semi: true,
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: false,
+};
